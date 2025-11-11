eecd6bcabdedf7e06e1bc9b9a91e885c41d2f7ed
Sodiqjon Sodiqov <sodiqjon@example.com>
2025-11-11 14:06:07 +0500

    fsdbgd

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const BOT_TOKEN = '8328125073:AAEWoSW-yjqgPLq4uLPEKGyemwa2lr47x6I';
 const CHAT_ID   = '-4935605017';
+const TELEGRAM_CHANNEL = 'https://t.me/megaaksiya2026';
 
 // Elementlar
 const openModal = document.getElementById('openModal');
 const formModal = document.getElementById('makon-modal');
 const closeModal = document.getElementById('closeModal');
 const makonForm = document.getElementById('makonForm');
-const successModal = document.getElementById('successModal');
 
 // Modalni ochish
 openModal.addEventListener('click', () => {
-  formModal.style.display = 'flex';
+  formModal.classList.add('show');  // 'show' klassini qo'shish
 });
 
 // Modalni yopish
 closeModal.addEventListener('click', () => {
-  formModal.style.display = 'none';
+  formModal.classList.remove('show'); // 'show' klassini olib tashlash
 });
 
 // Formani yuborish
@@ -33,6 +33,7 @@ makonForm.addEventListener('submit', async (e) => {
   const message = `📝 Yangi so‘rov:\n👤 Ism: ${name}\n📞 Telefon: ${phone}\n🌐 Sayt: 2 oq sayt`;
 
   try {
+    // Telegram botga xabar yuborish
     await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -43,31 +44,15 @@ makonForm.addEventListener('submit', async (e) => {
       })
     });
 
-    formModal.style.display = 'none';
-    successModal.style.display = 'flex';
+    // Formani yopish va reset qilish
+    formModal.classList.remove('show');
     makonForm.reset();
+
+    // Darhol Telegram kanalga yo'naltirish
+    window.location.href = TELEGRAM_CHANNEL;
+
   } catch (error) {
     alert("Xatolik yuz berdi, qayta urinib ko‘ring!");
     console.error(error);
   }
-});
- 
-
-
-
-
-
-
-
-
-
-
-// Modalni ochish
-openModal.addEventListener('click', () => {
-  formModal.classList.add('show');  // 'show' klassini qo'shish
-});
-
-// Modalni yopish
-closeModal.addEventListener('click', () => {
-  formModal.classList.remove('show'); // 'show' klassini olib tashlash
 });
